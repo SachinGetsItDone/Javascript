b1ac7b81d7fd9919d0ab8887b62b431212e8f36b
Sachin Sharma <239368358+SachinGetsItDone@users.noreply.github.com>
2026-06-03 15:09:29 +0000

    variables

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,18 @@
+const school_id = 123
+let school_mail = 'sachinsharma@google.com'
+var school_name = 'sachin'
+school = 'TPS'
+
+// school_id = 124 // not allowed 
+
+// school_mail = "sachin@google.com"
+
+// console.log(school_id);
+
+// console.log(school_mail);
+
+console.table([school_id, school_mail, school_name, school])
+
+/* 
+never use var because of the issues of block and function scope 
+*/
