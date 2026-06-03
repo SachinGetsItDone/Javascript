bdc1e1d2ebed4aae9ae7ab9d4c90148a1d04825e
Sachin Sharma <239368358+SachinGetsItDone@users.noreply.github.com>
2026-06-03 16:41:39 +0000

    datatypes

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // used for newer versions of javascripts
+
+// alert(3 + 3)
+
+let name = 'Sachin'
+let age = 20
+let is_logged_in = false
+
+// number => 2 to the power of 53
+// bigint 
+// string => ""
+// boolean
+// null // standalone value in js, it is a type of empty variable, khali hai 
+// undefined => unassigned value 
+// symbol used to find uniqueness 
+// object 
+
+console.log(typeof "sachin");
+console.log(typeof false);
+console.log(typeof 1); // this is a number in js
+console.log(typeof 1.22); // this is a number in js
+
+
