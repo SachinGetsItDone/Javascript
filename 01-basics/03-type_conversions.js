15d8f0bbb405f93d0d04847bffa36a7d77d6d2e5
Sachin Sharma <239368358+SachinGetsItDone@users.noreply.github.com>
2026-06-04 09:10:18 +0530

    Add files via upload

---
+++ 01-basics/03-type_conversions.js
@@ -0,0 +1,55 @@
+// let result = 33
+
+// console.log(typeof result)
+
+// let result = '33'
+// console.log(typeof result)
+
+// type conversion without inbuild funtion 
+// let result = 33
+// result = String(result)
+// console.log(typeof result)
+
+// let result = '33'
+// let value_in_number = Number(result)
+
+// console.log(typeof value_in_number)
+
+// let result = "33abc"
+// let value_in_number = Number(result)
+// console.log(typeof value_in_number) // number
+// console.log(value_in_number) // NaN => Not a Number
+
+// let is_logged_in = 1
+// let boolean_is_logged_in = Boolean(is_logged_in)
+// console.log(boolean_is_logged_in)
+
+// let IsLoggedIn = "a"
+
+// let boolean_is_logged_in = Boolean(IsLoggedIn)
+
+// console.log(boolean_is_logged_in)
+// console.log(typeof boolean_is_logged_in)
+
+// 1 => true, 0 => false
+// "" => false, " " => true, "sachin" => true
+// null => false, undefined => false, NaN => false
+
+
+// let random = undefined
+// let boolean_random = Boolean(random)
+// console.log(boolean_random) 
+// console.log(random)
+// let random = undefined
+// let var_random = String(random)
+// console.log(var_random, typeof var_random)
+
+let random = undefined
+let var_random = Number(random)
+console.log(var_random, typeof var_random)
+
+/*
+null => 0
+undefined => Nan
+NaN => 0
+*/
