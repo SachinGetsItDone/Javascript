d18241b643635916477911e1d8cfae3988e21db8
Sachin Sharma <239368358+SachinGetsItDone@users.noreply.github.com>
2026-06-04 14:57:02 +0000

    operations

--- 01-basics/03-type_conversions.js
+++ 01-basics/03-type_conversions.js
@@ -44,12 +44,51 @@
 // let var_random = String(random)
 // console.log(var_random, typeof var_random)
 
-let random = undefined
-let var_random = Number(random)
-console.log(var_random, typeof var_random)
+// let random = undefined
+// let var_random = Number(random)
+// console.log(var_random, typeof var_random)
+
+// let some_number = 33
+// let same_number = String(some_number)
+// console.log(same_number);
+// console.log(typeof same_number);
+
 
 /*
 null => 0
 undefined => Nan
 NaN => 0
 */
+
+// ****************Operations***********************
+
+// console.log(2 + 2);
+// console.log(2 + 3 + 4 * 3);
+
+// let string_1 = "sachin"
+// let string_2 = " sharma"
+// let concat = string_1 + string_2
+// console.log(concat);
+
+// console.log(+true);
+// console.log(+false);
+// console.log(+true);
+
+// console.log(+'s'); NaN
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); 
+// console.log(2/2);
+// console.log(2%2);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(((3 + 4) * 5) % 3);
+
+
+
+
+
