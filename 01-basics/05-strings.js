a2e7ff07150ef6dcdbc632025d8b7778f7ca3dca
Sachin Sharma <239368358+SachinGetsItDone@users.noreply.github.com>
2026-06-05 22:49:28 +0530

    string

---
+++ 01-basics/05-strings.js
@@ -0,0 +1,38 @@
+var string = "Hello World"
+// console.log(string.charAt(3))
+// string.charAt(3) = "a" // This will not work as strings are immutable in JavaScript
+// string[0] = "h" // This will also not work for the same reason
+
+// const a = "a";
+// const b = "b";
+// if (a < b) {
+//   // true
+//   console.log(`${a} is less than ${b}`);
+// } else if (a > b) {
+//   console.log(`${a} is greater than ${b}`);
+// } else {
+//   console.log(`${a} and ${b} are equal.`);
+// }
+
+// function are_equal_case_insensitive(str1, str2) {
+//   return str1.toLowerCase() === str2.toLowerCase();
+// }
+
+// console.log(are_equal_case_insensitive("Hello", "hello")); 
+// console.log(are_equal_case_insensitive("Hello", "sachin"));
+
+// const str = "sachin";
+// document.body.innerHTML = str.anchor("hello")
+
+// function str(str){
+//     return str.at(-1)
+// }
+// console.log(str("sachin"))
+
+
+let my_string = "my name is sachin sharma"
+// console.log(my_string.slice(4, 7))
+// console.log(my_string.split(" "))
+// console.log(my_string.trim())
+// console.log(my_string.toWellFormed())
+console.log(my_string.)
