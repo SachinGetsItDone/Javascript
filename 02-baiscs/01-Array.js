4d3e8c4f29fe280b821c2e361875276f28b29cf5
Sachin Sharma <239368358+SachinGetsItDone@users.noreply.github.com>
2026-06-08 22:21:16 +0530

    Array-1

--- 02-baiscs/01-Array.js
+++ 02-baiscs/01-Array.js
@@ -3,7 +3,7 @@ let myArr = [1, 2, 3, 4, 5, "sachin", "sharma"]
 // Shallow copy 
 // let copyArr = myArr
 // copyArr[2] = 0
-console.log(myArr);
+// console.log(myArr);
 // console.log(copyArr);
 
 // // Deep copy 
@@ -14,8 +14,27 @@ console.log(myArr);
 
 // Array methods
 
-myArr.push(4)
-myArr.push(4)
-console.log(myArr);
-myArr.pop()
+// myArr.push(4)
+// myArr.push(4)
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr)
+
+// myArr.unshift(10)
+// console.log(myArr);
+// myArr.shift()
+// console.log(myArr)
+// console.log(myArr.includes(10))
+// console.log(myArr.indexOf(3));
+
+
+const myNewArray = myArr.slice(1, 3);
+console.log(myNewArray)
 console.log(myArr)
+const myNewArray2 = myArr.splice(1, 3);
+console.log(myNewArray2)
+console.log(myArr)
+
+
+// Slice print the part of an array excluding the end 
+// Splice cut out a part of an array from the original one
