73cacc62cc6ecdd5ceed500a3025a9495e5b9c7b
Sachin Sharma <239368358+SachinGetsItDone@users.noreply.github.com>
2026-06-17 16:18:48 +0530

    hoisting

--- 03-functions/03-scopes.js
+++ 03-functions/03-scopes.js
@@ -1,22 +1,56 @@
 
 
+// if (true) {
+//     let a = 10
+//     const b = 20
+//     var c = 30
+// }
+
+
+// console.log(c);
+
+
+// let globalVar = "I am global"
+
+// function checkScope() {
+//     let localVar = "I am local"
+//     console.log(globalVar)
+//     console.log(localVar)
+// }
+// // console.log(localVar) // It will throw an error
+// checkScope()
+
+
+function one() {
+    const user = "Sachin";
+    function two() {
+        const game = "Ghost of tsushima";
+        // console.log(game);
+    }
+    // console.log(user);
+    two()
+}
+// console.log(user);
+one();
+
 if (true) {
-    let a = 10
-    const b = 20
-    var c = 30
+    const user = "Sachin"
+
+    if (user == "Sachin") {
+        const website = "youtube";
+        // console.log(user + website);
+    }
+    // console.log("Done");
 }
 
+console.log(addOne(5));
 
-console.log(c);
-
-
-let globalVar = "I am global"
-
-function checkScope() {
-    let localVar = "I am local"
-    console.log(globalVar)
-    console.log(localVar)
+function addOne(num) {
+    return num + 1
 }
-// console.log(localVar) // It will throw an error
-checkScope()
 
+const addTwo = function (num) {
+    return num + 2
+}
+
+console.log(addTwo(5));
