65b871c4c603eac113dddfdd6beb8fab3c470615
Sachin Sharma <239368358+SachinGetsItDone@users.noreply.github.com>
2026-06-21 12:38:34 +0530

    for of and in

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -6,5 +6,21 @@ const myObject = {
 }
 
 for (const key in myObject) {
-    console.log(key);
+    console.log(`${key} shortcut is for ${myObject[key]}`)
 }
+
+const programming = ["js", "rb", "py", "java"]
+
+for (const key in programming) {
+    console.log(`${key} = ${programming[key]}`)
+}
+
+// Map example using for...in loop:
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('FR', "France")
+
+for (const key in map) {
+    console.log(key); // This will not print anything because Maps are not iterable via for...in
+}
