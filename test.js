e84acf5b39b8d85e16a179d9a28dd3f279bb6588
Sachin Sharma <239368358+SachinGetsItDone@users.noreply.github.com>
2026-06-22 21:06:43 +0530

    test.js

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-console.log("hi chai")
+const session = {
+    username: "Sachin",
+    greetUser: function () {
+        return `Hello, ${this.username}!`;
+    },
+    greetUserArrow: () => {
+        console.log(this);
+
+        return `Hello, ${session.username}!`; // Buggy: returns "Hello, undefined!"
+    }
+};
+
+console.log(session.greetUserArrow());
+console.log(session.greetUser());
