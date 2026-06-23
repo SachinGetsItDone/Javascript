0e130b1cb090803d332dfe81ba7107d5dc661958
Sachin Sharma <239368358+SachinGetsItDone@users.noreply.github.com>
2026-06-23 09:52:26 +0530

    four and five

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -1,26 +1,25 @@
 const myObject = {
-    js: "Javascript",
-    cpp: "C++",
-    rb: "Ruby",
-    swift: "Swift"
-}
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+};
 
 for (const key in myObject) {
-    console.log(`${key} shortcut is for ${myObject[key]}`)
+    console.log(`${key} shortcut is for ${myObject[key]}`);
 }
 
-const programming = ["js", "rb", "py", "java"]
+const programming = ["js", "rb", "py", "java", "cpp"];
 
 for (const key in programming) {
-    console.log(`${key} = ${programming[key]}`)
+    console.log(programming[key]);
 }
 
-// Map example using for...in loop:
-const map = new Map()
-map.set('IN', "India")
-map.set('USA', "United States of America")
-map.set('FR', "France")
+const map = new Map();
+map.set('IN', "India");
+map.set('USA', "United States of America");
+map.set('FR', "France");
 
 for (const key in map) {
-    console.log(key); // This will not print anything because Maps are not iterable via for...in
-}
+    console.log(key);
+}
